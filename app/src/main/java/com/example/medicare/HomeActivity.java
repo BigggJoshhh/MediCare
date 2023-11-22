93489aaf103ff631eeaef23c75c43a829ee0e6fb
Jjoshhh <JOSH.22@ichat.sp.edu.sg>
2023-11-22 23:54:05 +0800

    Splash and Home Done

---
+++ app/src/main/java/com/example/medicare/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.medicare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.medicare.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
--- app/src/main/java/com/example/medicare/MainActivity.java
+++ app/src/main/java/com/example/medicare/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.medicare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
+
+        // Get a reference to the root layout of your splash screen
+        View splashLayout = findViewById(R.id.splashLayout);
+
+        // Set an OnClickListener to navigate to the home activity
+        splashLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the home activity when the splash screen is clicked
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
